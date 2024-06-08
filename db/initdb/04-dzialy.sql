0c7c008181ec209474d89a33e01b277f9cc2e3aa
Marek Tews <marek.tews@gmail.com>
2024-06-08 16:53:06 +0200

    Dodanie pola limitu pojazdów dla działów kongresowych

--- db/initdb/04-dzialy.sql
+++ db/initdb/04-dzialy.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `Dzialy` (
     `lang` varchar(32) NOT NULL DEFAULT 'pl',
     `name` varchar(128) NOT NULL,
     `password` varchar(32) NOT NULL,
+    `plimit` INT DEFAULT 3 NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`)
 ) 

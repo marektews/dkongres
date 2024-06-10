aaf85f742c9752cca62905a6bf5d00a4945a16d8
Marek Tews <marek.tews@gmail.com>
2024-06-10 18:26:03 +0200

    Dodanie pola 'tura' do tabeli Działy

--- db/initdb/04-dzialy.sql
+++ db/initdb/04-dzialy.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `Dzialy` (
     `name` varchar(128) NOT NULL,
     `password` varchar(32) NOT NULL,
     `plimit` INT DEFAULT 3 NOT NULL,
+    `tura` INT DEFAULT 1 NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`)
 ) 

664d7e9c8b0ac70dd531b1efdd07fa1f6414476c
Marek Tews <marek.tews@gmail.com>
2024-07-09 22:39:15 +0200

    Dodanie dwóch projektów: Czw i Przyjazdy

--- webroot/build.sh
+++ webroot/build.sh
@@ -19,6 +19,8 @@ git clone https://github.com/marektews/ia.git
 git clone https://github.com/marektews/pk.git
 git clone https://github.com/marektews/sektor.git
 git clone https://github.com/marektews/scanner.git
+git clone https://github.com/marektews/czw.git
+git clone https://github.com/marektews/przyjazdy.git
 
 cd /devel/admlogin
 npm install --no-fund
@@ -69,6 +71,14 @@ cd /devel/scanner
 npm install --no-fund
 npm run build
 
+cd /devel/czw
+npm install --no-fund
+npm run build
+
+cd /devel/przyjazdy
+npm install --no-fund
+npm run build
+
 # kopiowanie do lokalizacji webroot
 mkdir -p /webroot/admin/sra
 mkdir -p /webroot/admin/srp
@@ -89,6 +99,8 @@ mkdir -p /webroot/ia
 mkdir -p /webroot/pk
 mkdir -p /webroot/sektor
 mkdir -p /webroot/scanner
+mkdir -p /webroot/czw
+mkdir -p /webroot/przyjazdy
 
 cp -f -r /devel/sra/dist/* /webroot/sra
 cp -f -r /devel/srp/dist/* /webroot/srp
@@ -96,3 +108,5 @@ cp -f -r /devel/ia/dist/* /webroot/ia
 cp -f -r /devel/pk/dist/* /webroot/pk
 cp -f -r /devel/sektor/dist/* /webroot/sektor
 cp -f -r /devel/scanner/dist/* /webroot/scanner
+cp -f -r /devel/czw/dist/* /webroot/czw
+cp -f -r /devel/przyjazdy/dist/* /webroot/przyjazdy
